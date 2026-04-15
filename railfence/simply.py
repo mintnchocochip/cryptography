251dad25564820331510f0bb146d2e3d87452563
Manikandan <81812564+mintnchocochip@users.noreply.github.com>
2026-04-15 07:58:06 +0530

    more

--- railfence/simply.py
+++ railfence/simply.py
@@ -6,5 +6,10 @@ def encrypt(plaintext, depth):
     return "".join("".join(rails))
 
 def decryption(cipher, depth):
+    n = len(cipher)
     pattern = list(range(depth)) + list(range(dpeth-2,0,-1))
     indices = sorted(range(n), key = lembda i: pattern[i%len(pattern)])
+    result = [""]*n
+    for i,ch in zip(indices, cipher):
+        result[i] = ch
+    return "".join(result)
